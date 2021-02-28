5546b3c81b264fa8b3a8b13acbeee4845ce21653
niuyh <2459100359@qq.com>
2021-02-28 16:17:52 +0800

    删除注释

--- src/main/java/com/tuling/designpattern/singleton/v4/EnumSingletonTest.java
+++ src/main/java/com/tuling/designpattern/singleton/v4/EnumSingletonTest.java
@@ -2,10 +2,6 @@ package com.tuling.designpattern.singleton.v4;
 
 import java.io.*;
 
-/**
- * @author 腾讯课堂-图灵学院  郭嘉
- * @Slogan 致敬大师，致敬未来的你
- */
 public class EnumSingletonTest {
     public static void main(String[] args) throws IOException {
         EnumSingleton instance=EnumSingleton.INSTANCE;
